2b1a04c7c24ba88d87e102a0c11995bf5e144bee
mahdi azizi <m.azizi1376@gmail.com>
2021-04-16 17:20:19 +0430

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public void sumaverageelements(int[] arr, int size)
+        {
+            int sum = 0;
+            int average = 0;
+                 for (int i = 0; i < size; i++)
+            {
+                sum += arr[i];
+            }
+            average = sum / size;
+            Console.WriteLine("jam araye :" + sum);
+            Console.WriteLine("miyangin araye ha :" + average);
+            Console.ReadLine();
+        }
+        public static void Main(string[] args)
+        {
+            int size;
+            Console.WriteLine("size ra vered kinid:");
+            size = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[size];
+            Console.WriteLine("adad ra vared kinid:");
+            for (int i = 0; i < size; i++)
+
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int len = a.Length;
+            Program pg = new Program();
+            pg.sumaverageelements(a,len);
+
+
+        }
+    }
+}
